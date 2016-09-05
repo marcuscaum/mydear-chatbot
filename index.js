02405098ffb47d7f3cbf691d98b6f276a30edbcc
marcuscaum <marcuslucas.jb@gmail.com>
2016-09-05 14:55:11 -0300

    welcome body parser

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const PAGE_ACCESS_TOKEN = "EAAEQ7IqZAuXoBAHdSxZBnQhnkZCWH8R8mAxQtKMcmZBZALMpqGKm5zil3G8WWMJAXbmqRIJndeMYscvBW9ZCyUxfnFYS1hNmFm2X9PAFoIWE7yp7ioplTZApgto6DaqKWkcQzhkZCFMi58LvppUtP53j3ZBbdhalTR6lH11rjoWFUngZDZD";
 const fetch = require('node-fetch');
 const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const jsonParser = bodyParser.json();
+
 
 fetch('http://api.themoviedb.org/3/movie/now_playing?api_key=ea063f4f9f9c96a700b99b9737a83c80')
   .then(function(response){
@@ -13,54 +17,6 @@ fetch('http://api.themoviedb.org/3/movie/now_playing?api_key=ea063f4f9f9c96a700b
     });
   });
 
-
-let app = express();
-
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'keep_this_secret') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
-});
-
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object == 'page') {
-    // Iterate over each entry
-    // There may be multiple if batched
-    data.entry.forEach(function(pageEntry) {
-      var pageID = pageEntry.id;
-      var timeOfEvent = pageEntry.time;
-
-      // Iterate over each messaging event
-      pageEntry.messaging.forEach(function(messagingEvent) {
-        if (messagingEvent.optin) {
-          receivedAuthentication(messagingEvent);
-        } else if (messagingEvent.message) {
-          receivedMessage(messagingEvent);
-        } else if (messagingEvent.delivery) {
-          receivedDeliveryConfirmation(messagingEvent);
-        } else if (messagingEvent.postback) {
-          receivedPostback(messagingEvent);
-        } else {
-          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know you've
-    // successfully received the callback. Otherwise, the request will time out.
-    res.sendStatus(200);
-  }
-});
 
 function receivedMessage(event) {
   var senderID = event.sender.id;
@@ -143,4 +99,52 @@ function callSendAPI(messageData) {
   });
 }
 
-app.listen(process.env.PORT || 8080);
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'keep_this_secret') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook', jsonParser, function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          receivedMessage(messagingEvent);
+        } else if (messagingEvent.delivery) {
+          receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          receivedPostback(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
+  }
+});
+
+
+app.listen(process.env.PORT || 3232);
