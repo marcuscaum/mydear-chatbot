8f2824bba748c92be3b17a230ba1258100197b4f
marcuscaum <marcuslucas.jb@gmail.com>
2016-09-05 13:54:15 -0300

    Procfile and express

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-let fetch = require('node-fetch');
+const fetch = require('node-fetch');
+const express = require('express');
 
 fetch('http://api.themoviedb.org/3/movie/now_playing?api_key=ea063f4f9f9c96a700b99b9737a83c80')
   .then(function(response){
@@ -10,3 +11,12 @@ fetch('http://api.themoviedb.org/3/movie/now_playing?api_key=ea063f4f9f9c96a700b
       });
     });
   });
+
+
+let app = express();
+
+app.get('/hello', function(req, res) {
+  res.send('world')
+})
+
+app.listen(process.env.PORT || 8080);
